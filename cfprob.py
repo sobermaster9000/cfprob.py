d557c27554a442cd3105a37606986627bb7e697a
sobermaster9000 <awebro207@gmail.com>
2026-07-23 21:59:55 +0800

    Implement basic function for suggesting problems

--- cfprob.py
+++ cfprob.py
@@ -1,4 +1,7 @@
+from sys import argv
 import argparse
+import requests
+from random import randint
 # import json
 
 ALLOWED_RATINGS = [x*100 for x in range(8, 36)]
@@ -41,13 +44,21 @@ ALLOWED_TAGS = [
     "schedules",
 ]
 
+CODEFORCES_URL = "https://codeforces.com/"
+API_ENDPOINT = CODEFORCES_URL + "api/"
+
 description = "A command line utility for suggesting problem from Codeforces, with limited ThemeCP support. Learn more about ThemeCPs here: https://codeforces.com/blog/entry/136704."
 
 parser = argparse.ArgumentParser(description=description)
 parser.add_argument("-r", "--rating", type=int, help=f"sets rating of problem to suggest; valid values for RATING:\n{', '.join([str(x) for x in ALLOWED_RATINGS])}")
 parser.add_argument("-t", "--tag", type=str, help=f"sets tag of problem to suggest; valid values for TAG (enclose multi-worded ones in quotes):\n{', '.join(ALLOWED_TAGS)}")
 parser.add_argument("-l", "--level", type=int, help="sets ThemeCP level, and suggests 4 problems based on that level\nthis is a standalone argument and cannot be used with either -r/--rating or -t/--tag; valid values for LEVEL are integers from 1 to 109")
+parser.add_argument("-u", "--user", type=str, help="sets user handle such that the suggested problem/s are ones that have not yet been solved by this user")
 args = parser.parse_args()
+
+# if len(argv) == 1:
+#     parser.print_help()
+#     exit(0)
 
 if args.rating is not None and args.rating not in ALLOWED_RATINGS:
     # print allowed values in two columns
@@ -64,10 +75,27 @@ if args.level is not None:
     if args.level < 1 or args.level > 109:
         parser.error("argument value for -l/--level is invalid")
 
-# also ask for user handle
-
 # if no user handle given
 # suggest a random problem based on given rating/tag
 # else
 # preload previously solved problems
 # suggest a random unsolved problem based on given rating/tag
+
+def suggest_problem(rating=None, tag="", user_handle=None):
+    response = requests.get(API_ENDPOINT + "problemset.problems", params={"tags": tag})
+    if response.status_code != requests.codes.ok:
+        response.raise_for_status()
+    problems = response.json()["result"]["problems"]
+    idx = randint(0, len(problems)//4)
+    problem = None
+    while True:
+        problem = problems[idx]
+        if rating is not None:
+            if problem["rating"] == rating:
+                break
+        else:
+            break
+        idx = (idx + 1) % len(problems)
+    return CODEFORCES_URL + f"problemset/problem/{problem["contestId"]}/{problem["index"]}"
+
+print(suggest_problem(rating=args.rating, tag=args.tag, user_handle=args.user))
