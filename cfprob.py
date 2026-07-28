a027e9ca043d219f706666ee46c886a5e534355c
sobermaster9000 <awebro207@gmail.com>
2026-07-28 15:32:33 +0800

    Implement feature for ThemeCP problem suggestions

--- cfprob.py
+++ cfprob.py
@@ -48,14 +48,14 @@ API_ENDPOINT = CODEFORCES_URL + "api/"
 
 DESCRIPTION = "A command line utility for suggesting problem from Codeforces, with limited ThemeCP support. Learn more about ThemeCPs here: https://codeforces.com/blog/entry/136704."
 
-def suggest_problem(rating=None, tag=None, user_handle=None):
-    if rating is not None:
-        print("[*] Rating present:", rating)
-    if tag is not None:
-        print("[*] Tag present:", tag)
-    if user_handle is not None:
-        print("[*] User handle is present:", user_handle)
+def get_themecp_ratings(level):
+    lines = []
+    with open("themecp_level_sheet.csv", "r") as file:
+        lines = [line.strip()  for line in file.readlines()]
+    _, time, perf, p1, p2, p3, p4 = map(int, lines[level].split(","))
+    return p1, p2, p3, p4
 
+def suggest_problem(rating=None, tag=None, user_handle=None, themecp_problems=None):
     solved_problems = set()
     if user_handle is not None:
         response = requests.get(API_ENDPOINT + "user.status", params={"handle": user_handle})
@@ -89,13 +89,16 @@ def suggest_problem(rating=None, tag=None, user_handle=None):
             continue
         break
 
+    if themecp_problems is not None:
+        themecp_problems.add((problem["contestId"], problem["index"]))
+
     return CODEFORCES_URL + f"problemset/problem/{problem["contestId"]}/{problem["index"]}"
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=DESCRIPTION)
     parser.add_argument("-r", "--rating", type=int, help=f"sets rating of problem to suggest; valid values for RATING:\n{', '.join([str(x) for x in ALLOWED_RATINGS])}")
     parser.add_argument("-t", "--tag", type=str, help=f"sets tag of problem to suggest; valid values for TAG (enclose multi-worded ones in quotes):\n{', '.join(ALLOWED_TAGS)}")
-    parser.add_argument("-l", "--level", type=int, help="sets ThemeCP level, and suggests 4 problems based on that level\nthis is a standalone argument and cannot be used with either -r/--rating or -t/--tag; valid values for LEVEL are integers from 1 to 109")
+    parser.add_argument("-l", "--level", type=int, help="sets ThemeCP level, and suggests 4 problems based on that level; this cannot be used with -r/--rating; valid values for LEVEL are integers from 1 to 109")
     parser.add_argument("-u", "--user", type=str, help="sets user handle such that the suggested problem/s are ones that have not yet been solved by this user")
     args = parser.parse_args()
     
@@ -107,21 +110,46 @@ if __name__ == "__main__":
         # print allowed values in two columns
         parser.error("argument value for -t/--tag is invalid")
     
-    if args.level is not None and (args.rating is not None or args.tag is not None):
-        parser.error("argument -l/--level cannot be used with either -r/--rating or -t/--tag")
+    if args.level is not None and args.rating is not None:
+        parser.error("argument -l/--level cannot be used with -r/--rating")
     
     if args.level is not None:
         if args.level < 1 or args.level > 109:
             parser.error("argument value for -l/--level is invalid")
     
     try:
-        problem_link = suggest_problem(rating=args.rating, tag=args.tag, user_handle=args.user)
-        print(f"[+] Suggested problem: {problem_link}")
+        if args.level is None:
+            if args.rating is not None:
+                print("[*] Rating present:", args.rating)
+            if args.tag is not None:
+                print("[*] Tag present:", args.tag)
+            if args.user is not None:
+                print("[*] User handle is present:", args.user)
+
+            problem_link = suggest_problem(rating=args.rating, tag=args.tag, user_handle=args.user)
+            print(f"[+] Suggested problem: {problem_link}")
+        else:
+            print("[*] ThemeCP level present:", args.level)
+            if args.tag is not None:
+                print("[*] Tag present:", args.tag)
+            if args.user is not None:
+                print("[*] User handle is present:", args.user)
+
+            themecp_ratings = get_themecp_ratings(args.level)
+            themecp_problems = set()
+
+            print("[+] Suggested problems:")
+            print("RATING\t|\tLINK")
+            print("-----------------------------------------------------------------")
+
+            for rating in themecp_ratings:
+                problem_link = suggest_problem(rating=rating, user_handle=args.user, themecp_problems=themecp_problems)
+                print(f"{rating}\t|\t{problem_link}")
+
     except Exception as e:
         print("[!] An error occured while searching for problem/s:")
         print(e)
 
 # TODO
-# implement feature for themecp problem suggestions
 # add a flag like --set-user to save user to local file and eliminate repetitive flag setting
 # allow -t/--tag to receive multiple tags
